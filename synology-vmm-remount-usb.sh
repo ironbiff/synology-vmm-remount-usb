4ccdfd40f96ed4f195f009c377ff21c0460d0a9a
ironbiff <github@minus42.de>
2025-02-07 20:26:41 +0100

    Update synology-vmm-remount-usb.sh

--- synology-vmm-remount-usb.sh
+++ synology-vmm-remount-usb.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-DEVICE_ID="VENDOR_ID" #like 1cf1 
-VM_NAME="VIRSH_NAME" #like d25ac3aa-76ff-4bb8-972e-dee62d3f914d
+DEVICE_ID="$VENDOR_ID$"
+VM_NAME="$VIRSH_VM_NAME$"
 
-if ! virsh dumpxml d25ac3aa-76ff-4bb8-972e-dee62d3f914d | grep -q "$DEVICE_ID"; then
-#    echo "USB-Dongle nicht gefunden. Versuche, ihn erneut zu verbinden..."
-#    synoservice --restart synousbservice
-    echo "Dongle erneut mounten für VM $VM_NAME..."
-    virsh attach-device "$VM_NAME" /var/services/homes/ironbiff/synology/usb_conbee/usb_conbee.xml
+if ! virsh dumpxml "$VM_NAME" | grep -q "$DEVICE_ID"; then
+    echo "Remounting USB dongle for VM $VM_NAME..."
+    virsh attach-device "$VM_NAME" /var/services/homes/$USERNAME$/synology/usb_conbee/usb_conbee.xml
 fi
