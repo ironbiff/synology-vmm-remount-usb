19a969dc5d47bf54661b3d5a73f27a1cf9958d45
ironbiff <github@minus42.de>
2025-02-07 21:00:06 +0100

    Update synology-vmm-remount-usb.sh

--- synology-vmm-remount-usb.sh
+++ synology-vmm-remount-usb.sh
@@ -4,5 +4,5 @@ VM_NAME="$VIRSH_VM_NAME$"
 
 if ! virsh dumpxml "$VM_NAME" | grep -q "$DEVICE_ID"; then
     echo "Remounting USB dongle for VM $VM_NAME..."
-    virsh attach-device "$VM_NAME" /var/services/homes/$USERNAME$/synology/usb_conbee/usb_conbee.xml
+    virsh attach-device "$VM_NAME" /var/services/homes/ironbiff/synology/usb_conbee/usb_conbee.xml
 fi
